3c06c0c073ecbebb4114298972d7f828d124237f
Robert Valk <robert.valk@sixtree.com.au>
2014-03-13 13:25:33 +1100

    hard-code to 2 go agents by default.

--- attributes/default.rb
+++ attributes/default.rb
@@ -13,4 +13,4 @@ default[:go][:agent][:server_host]           = "127.0.0.1"
 default[:go][:agent][:auto_register]         = false
 default[:go][:agent][:auto_register_key]     = "default_auto_registration_key"
 # Install this many agent instances on a box - default is one per CPU
-default[:go][:agent][:instance_count]        = node[:cpu][:total]
+default[:go][:agent][:instance_count]        = 2 # node[:cpu][:total]
