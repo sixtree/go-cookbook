571ee700fab06e0364cc8b8fb9983fa85762b224
Robert Valk <robert.valk@sixtree.com.au>
2014-03-13 13:19:30 +1100

    No need to force server IP address

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,5 +1,3 @@
-# Since we're running on a single node we can just use 127.0.0.1 for the server IP. Search isn't necessary
-node.default[:go][:server] = '127.0.0.1'
 include_recipe "go::server"
 include_recipe "go::agent"
 
